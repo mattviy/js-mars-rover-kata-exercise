99edb7d6e90c407c70b59f583768dc0b32ca4f3d
mattviy <mrmhamers@gmail.com>
2018-10-15 22:15:47 +0200

    added backward bonus function

--- javascripts/app.js
+++ javascripts/app.js
@@ -77,11 +77,49 @@ function moveForward(rover){
   }
 }
 
+function moveBackward(rover){
+  rover.travelLog.push(rover.xPosition + "," + rover.yPosition);
+  switch(rover.direction){
+    case 'N':
+      if (rover.yPosition == 10){
+        console.log("Rover reached the boundary, can't move forward.");
+      } else {
+        rover.yPosition++;
+      }
+      break;
+    case 'E':
+      if (rover.xPosition == 0){
+        console.log("Rover reached the boundary, can't move forward.");
+      } else { 
+        rover.xPosition--;
+      }
+      break;
+    case 'S':
+      if (rover.yPosition == 0){
+        console.log("Rover reached the boundary, can't move forward.");
+      } else {
+        rover.yPosition--;
+      }
+      break;
+    case 'W':
+    if (rover.xPosition == 10){
+      console.log("Rover reached the boundary, can't move forward.");
+    } else {
+      rover.xPosition++;
+    }
+    break;
+  }
+}
+
 function commands(rover, input){
   for (var i = 0; i < input.length; i++){
     switch(input[i]){
       case 'f':
       moveForward(rover);
+      console.log(rover.yPosition + ", " + rover.xPosition);
+      break; 
+      case 'b':
+      moveBackward(rover);
       console.log(rover.yPosition + ", " + rover.xPosition);
       break; 
       case 'r':
@@ -96,5 +134,5 @@ function commands(rover, input){
   }
 }
 
-commands(myRover,"llfffflfffflfffflfffff");
+commands(myRover,"llffbbb");
 console.log(myRover.travelLog);
